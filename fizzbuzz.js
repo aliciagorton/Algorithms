1f75f26d5407af4579d3960c929ff20d555806b2
Alicia Gorton <apersigehl@gmail.com>
2021-01-06 16:43:52 -0800

    first commit adding pseudo code

---
+++ fizzbuzz.js
@@ -0,0 +1,19 @@
+// Given a random positive integar, 'num', print all of the numbers from 1 to 'num'
+// But for multiples of 3, printe 'Fizz' instead of the number 
+// And for multiples of 5, print 'Buzz' instead of the number
+// And for multiple of 3 and 5, print 'FizzBuzz' instead of the number 
+
+function fizzBuzz(num)(
+ 
+    // set up a for loop starting from 1 going until 'num'
+
+    // check if the current iteration (i) is a multiple of 3 and 5, if so print 'FizzBuzz'
+
+    // check if the current iteration (i) is a multiple of 3, if so print 'Fizz'
+
+    // check if the current iteration (i) is a multiple of 5, if so print 'Buzz'
+
+    // else, print 1 
+)
+
+fizzBuzz(25);
