25d003202b0eade7ec98d0f8ea31056e93fccbb5
Alicia Gorton <apersigehl@gmail.com>
2021-01-10 14:05:15 -0800

    adding solved code

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,17 +3,28 @@
 // And for multiples of 5, print 'Buzz' instead of the number
 // And for multiple of 3 and 5, print 'FizzBuzz' instead of the number 
 
-function fizzBuzz(num)(
+function fizzBuzz(num){
  
     // set up a for loop starting from 1 going until 'num'
+    for (var i = 1; i <= num; i++) {
 
     // check if the current iteration (i) is a multiple of 3 and 5, if so print 'FizzBuzz'
-
+    if(i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    }
     // check if the current iteration (i) is a multiple of 3, if so print 'Fizz'
-
+    else if (i % 3 === 0 {
+        console.log ("Fizz");
+    }
     // check if the current iteration (i) is a multiple of 5, if so print 'Buzz'
-
+    else if (i % 5 === 0 {
+        console.log ("Buzz");
+    }
     // else, print 1 
-)
+    else {
+        console.log(i);
+    }
+}   
+}
 
 fizzBuzz(25);
